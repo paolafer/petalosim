1e7e01b990781b41a7bec93117779d063d986ba4
Paola Ferrario <paola.ferrario@gmail.com>
2023-12-13 16:41:06 +0100

    Create a teflon block for 3x3 SiPM holes

---
+++ source/geometries/TeflonBlockHamamatsu3x3.h
@@ -0,0 +1,54 @@
+// ----------------------------------------------------------------------------
+// petalosim | TeflonBlockHamamatsu3x3.h
+//
+// Teflon block used with the Hamamatsu VUV tiles,
+// with 9 SiPMs coupled to 1 hole and 30 mm of length.
+//
+// The PETALO Collaboration
+// ----------------------------------------------------------------------------
+
+#ifndef TEFLON_HAMA_2x2_H
+#define TEFLON_HAMA_2x2_H
+
+#include "nexus/GeometryBase.h"
+
+class G4Material;
+namespace nexus
+{
+class IonizationSD;
+}
+
+using namespace nexus;
+
+class TeflonBlockHamamatsu3x3: public GeometryBase
+{
+public:
+  // Constructor
+  TeflonBlockHamamatsu3x3();
+  //Destructor
+  ~TeflonBlockHamamatsu3x3();
+
+  void Construct();
+
+  void SetHoleMaterial(G4Material* mat);
+  void SetIoniSD(IonizationSD* ionisd);
+  void SetMaxStepSize(G4double step_size);
+
+  G4double GetTeflonThickness() const;
+
+ private:
+
+  G4Material* mat_;
+  IonizationSD* ionisd_;
+  G4double teflon_block_thick_;
+  G4double max_step_size_;
+
+
+};
+
+inline void TeflonBlockHamamatsu3x3::SetHoleMaterial(G4Material* mat) {mat_ = mat;}
+inline void TeflonBlockHamamatsu3x3::SetIoniSD(IonizationSD* ionisd) {ionisd_ = ionisd;}
+inline void TeflonBlockHamamatsu3x3::SetMaxStepSize(G4double step_size) {max_step_size_ = step_size;}
+inline G4double TeflonBlockHamamatsu3x3::GetTeflonThickness() const {return teflon_block_thick_;}
+
+#endif
